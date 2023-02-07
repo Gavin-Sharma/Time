944196d6660c5d5184428c1a8307572ff0d226c9
Ravi Walberg <rrwalberg@gmail.com>
2023-02-07 14:48:43 -0800

    pushed even more updates

--- nodelogin/login.js
+++ nodelogin/login.js
@@ -1,17 +1,17 @@
-const app = require('express')(); 
+const express = require('express');
 const basicAuth = require('express-basic-auth');
 const bodyParser = require('body-parser'); // middleware
+const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const credentials = [ {'test': 'test'},{'admin': 'password'}];
 
-
 app.get('/', function(request, response){ 
-    response.sendFile(__dirname + '/login.html') //reposond by send a static file of index.htmls
+    response.sendFile(__dirname + '/static/login.html') //reposond by send a static file of index.htmls
 });
 
 app.get('/home', function(request, response){ 
-    response.sendFile(__dirname + '/homepage.html') //reposond by send a static file of index.htmls
+    response.sendFile(__dirname + '/static/homepage.html') //reposond by send a static file of index.htmls
 });
 
 app.post('/home', (req, res) => {
@@ -39,10 +39,18 @@ function myAuthorizer(username, password) {
 
 
     if (myAuthorizer(username, password)) {
-        res.sendFile(__dirname + "/homepage.html")
+        res.sendFile(__dirname + "/static/homepage.html")
     } else { res.send('You are not a user') }
 
 
 });
+
+app.use(express.static(__dirname + '/static', {
+    setHeaders: function (res, path) {
+      if (path.endsWith(".css")) {
+        res.set("Content-Type", "text/css");
+      }
+    }
+}));
 
 app.listen(3000);
