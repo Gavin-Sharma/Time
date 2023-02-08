157ed18c0421b783fda0effafcca32ef49cca273
Gavin Sharma <50966530+wow-gavin-codes-fast@users.noreply.github.com>
2023-02-08 01:44:08 -0800

    finished the view data

--- view_data/view_data.js
+++ view_data/view_data.js
@@ -1,33 +1,43 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-
+const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
 const app = express();
+
 const url = "mongodb://root:root@mongo_db:27017/";
-app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function(request, response){
-    response.sendFile(__dirname + '/index.html')
-});
+const path = require('path')
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
-app.use(express.static(__dirname + '/static', {
-    setHeaders: function (res, path) {
-      if (path.endsWith(".css")) {
-        res.set("Content-Type", "text/css");
-      }
-    }
-}));
-
-app.get('/data', function(req, res){
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        var dbo = db.db("analytics");
-        dbo.collection("analytics_mean").findOne({}, function(err, result) {
-            if (err) throw err;
-            res.json(result);
-            db.close();
-        });
-    });
+app.get("/", async (req, res) => {
+  try {
+    const client = await MongoClient.connect(url, { useUnifiedTopology: true });
+    const db = client.db("analytics");
+    const analyticsData = await db.collection("analytics_mean").findOne({});
+    res.send(`
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Show Data</title>
+          <link rel="stylesheet" href="./static/style.css">
+        </head>
+        <body>
+          <h1>Show Data</h1>
+          <div id="dataContainer">
+            Exercise Mean: ${analyticsData.exercise_mean}<br>
+            Homework Mean: ${analyticsData.homework_mean}<br>
+            Sleep Mean: ${analyticsData.sleep_mean}<br>
+            Phone Mean: ${analyticsData.phone_mean}<br>
+            Family Mean: ${analyticsData.family_mean}<br>
+            School Mean: ${analyticsData.school_mean}
+          </div>
+        </body>
+      </html>
+    `);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 app.listen(8004);
