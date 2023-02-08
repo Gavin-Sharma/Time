64c504ed5afe6f25ccf404931ff03c916388c9fe
Ravi Walberg <rrwalberg@gmail.com>
2023-02-07 23:28:32 -0800

    doing setup

--- view_data/view_data.js
+++ view_data/view_data.js
@@ -6,4 +6,12 @@ app.get('/', function(request, response){
     response.sendFile(__dirname + '/index.html')
 });
 
+app.use(express.static(__dirname + '/static', {
+    setHeaders: function (res, path) {
+      if (path.endsWith(".css")) {
+        res.set("Content-Type", "text/css");
+      }
+    }
+}));
+
 app.listen(8004);
