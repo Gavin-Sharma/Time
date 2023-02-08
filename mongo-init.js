440df98dee662d9278950d432e88a781ef468a41
your name <chanwoooolee@gmail.com>
2023-02-07 16:10:02 -0800

    updated mongo configuration files

--- mongo-init.js
+++ mongo-init.js
@@ -1,14 +1,12 @@
-// db = conn.getDB("project1");
-db = db.getSiblingDB('project1')
+var db = connect("mongodb://root:root@localhost:27017/");
 
-db.createUser({
-    user: "root",
-    pwd: "root",
-    roles: [
-        {
-            role: "readWrite",
-            db: "project1"
-        }
-    ]
-});
+db = db.getSiblingDB('new_db');
 
+db.createUser(
+    {
+        user: "admin",
+        pwd: "admin",
+        roles: [ { role: "readWrite", db: "new_db"} ],
+        passwordDigestor: "server",
+    }
+)
